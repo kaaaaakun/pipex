2ab50790c21252f5124997ce18878c80a31b1a42
kaaaaakun_ <kaaaaakun_@kaaaaakun-noMacBook-Air.local>
2023-08-10 00:14:36 +0900

    完成した！！

--- fork.c
+++ fork.c
@@ -5,75 +5,17 @@
 int exec(int argc);
 char *getpath(char *command);
 
-//int main() 
-//{
-//	  pid_t pid;
-//	  pid_t pid2;
-//	  time_t t;
-//	  int status;
-//
-//	pid_t child1;
-//    child1 = fork(); 
-//	if (child1 == 0)
-//		exec(2);
-//	else
-//		waitpid(child1, &status, 0);
-//	printf("end\n");
-//    return 0; 
-//}
-
-#include <stdio.h>
-#include <unistd.h>
-
-int exec(int argc)
-{
-  char    command[] = "/bin/cat";
-  char    *exargv[] = {"cat", "-n", "a", NULL};
-  char    *exenvp[] = {NULL};
-  int     return_code = 0;
-
-  if (argc == 2) {
-    /* 実行時引数（パス名）をexecveの引数にする */
- //   exargv[2] = argv[1];
-    if (execve(command, exargv, exenvp) == -1) {
-      printf("%sコマンドが実行できませんでした\n", command);
-      perror("");
-      return_code = 1;
-    }
-  }
-  else {
-    printf("実行時引数の数が不当です\n");
-    return_code = 2;
-  }
-  printf("引数２個の時にはここには来ない");
-  return return_code;
-}
-
 #include <stdlib.h>
 #include "libft.h"
 #include <stdio.h>
 #include <fcntl.h>
      #include <unistd.h>
-int main(int argc,char *argv[])
+void	childfork(char **argv, int *pipefd)
 {
-	pid_t pid;
-	char *path;
-	char **command;
-	int status;
-	int fd;
-	int pipefd[2];
+	int		fd;
+	char	*path;
+	char	**command;
 
-	pipe(pipefd);
-	    // プロセスを作成する
-    pid = fork();
-    // プロセスの作成に失敗
-    if ( pid < 0 ) {
-        perror("fork()");
-        return -1;
-    }
-
-	if (pid == 0)
-	{
 	command = ft_split(argv[2], ' ');
 	for (int i = 0;command[i] != NULL;i++)
 	{
@@ -87,15 +29,16 @@ int main(int argc,char *argv[])
 	fd = pipefd[1];
 	dup2(fd, 1);
 	close(fd);
+	close(pipefd[0]);
 	execve(path, command, NULL);
-		}
-	else
-	{ // 親プロセスの場合は、作成した子プロセスのPIDがリターンされる
-        waitpid(pid, &status, 0);
+}
 
-    pid = fork();
-	if (pid == 0)
-	{
+void	parentfork(char **argv, int *pipefd)
+{
+	int		fd;
+	char	*path;
+	char	**command;
+	
 	command = ft_split(argv[3], ' ');
 	for (int i = 0;command[i] != NULL;i++)
 	{
@@ -106,22 +49,38 @@ int main(int argc,char *argv[])
 	fd = pipefd[0];
 	dup2(fd, STDIN_FILENO);
 	close(fd);
-	fd = open("out", O_CREAT | O_WRONLY, 0666);
+	fd = open(argv[4], O_CREAT | O_TRUNC | O_WRONLY , 0660);
 	dup2(fd, 1);
 	close(fd);
+	close(pipefd[1]);
 	execve(path, command, NULL);
+}
+
+int main(int argc,char *argv[])
+{
+	pid_t pid;
+	int status;
+	int pipefd[2];
+
+	if (pipe(pipefd) < 0)
+	{
+        perror ("pipe()");
+		return (0);
 	}
-	waitpid(pid, &status, 0);
-        // 子プロセスの終了を待つ
-//        printf ("child process (PID = %d) finished\n", pid);
-//		dup2(fd, STDIN_FILENO);
-//		close(fd);
-//		dup2(fd, 1);
-//		close(fd);
-//		command = ft_split(argv[3], ' ');
-//	path = getpath(ft_strjoin("/", command[0]));
-//
-//	execve(path, command, NULL);
+    pid = fork();
+    if (pid < 0)
+	{
+        perror("fork()");
+        return (0);
+    }
+	if (pid == 0)
+	{
+		childfork(argv, pipefd);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		parentfork(argv, pipefd);
     }
 	return (0);
 }
